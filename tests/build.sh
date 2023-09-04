a0e3e46cc6d6e13fe2fe94126c534672272dd0b8
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-04 17:53:13 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -2,8 +2,8 @@
 
 RELEASE=$(git describe --abbrev=0 --tags)
 
-sed -i "s~ARG TARGETARCH~ARG TARGETARCH=amd64~g" Dockerfile.release
+sed -i "s~ARG TARGETARCH~ARG TARGETARCH=amd64~g" Dockerfile
 
-sed -i "s~ARG RELEASE~ARG RELEASE=${RELEASE}~g" Dockerfile.release
+sed -i "s~ARG RELEASE~ARG RELEASE=${RELEASE}~g" Dockerfile
 
-docker buildx build . -f Dockerfile.release --output type=docker,name=elestio4test/minio:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/minio:latest | docker load
