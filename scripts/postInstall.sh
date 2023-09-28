9736c75b165b4274920e91192bfe1c382a08d7f4
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-09-28 17:58:33 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,1 +1,8 @@
-# nothing to do here ...
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#Install MC (minio client) and preconfigure it
+wget https://dl.min.io/client/mc/release/linux-amd64/mc;
+chmod +x mc;
+mv mc /bin/mc;
+mc alias set minio http://172.17.0.1:9000 root ${SOFTWARE_PASSWORD} --api S3v4
